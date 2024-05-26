5285fb1797e289beb55eea03a73f6f825a3aa24d
snith nhotprasa <lesnith@gmail.com>
2024-05-27 01:07:45 +0200

    ajout de del bookmark et back button dans bookmark new

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :lists, only: %i[index show create] do
+  resources :lists do
     resources :bookmarks, only: %i[new create]
   end
-    resources :bookmarks, only: %i[destroy]
+    resources :bookmarks, only: [:destroy]
   # root "posts#index"
 end
